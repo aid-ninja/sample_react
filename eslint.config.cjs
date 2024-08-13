36c614e40b5edf4add82a77507073705b2237c2a
aid-ninja <aid.idrizovic@ninjatrader.com>
2024-08-13 18:36:59 -0500

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,153 +1,32 @@
 module.exports = {
-  root: true,
+  root: true, // Indicates that this is the root configuration file
   env: {
-    node: true,
-    es6: true,
+    browser: true, // Define global variables for browser environments
+    es2021: true, // Enable ES2021 features
   },
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  ignorePatterns: [
-    'node_modules/*',
-    'public/mockServiceWorker.js',
-    'generators/*',
+  extends: [
+    'eslint:recommended', // Use ESLint’s recommended rules
+    'plugin:react/recommended', // Use recommended rules from the React plugin
+    'plugin:react-hooks/recommended', // Use recommended rules for React Hooks
   ],
-  extends: ['eslint:recommended'],
-  plugins: ['check-file'],
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      settings: {
-        react: { version: 'detect' },
-        'import/resolver': {
-          typescript: {},
-        },
-      },
-      env: {
-        browser: true,
-        node: true,
-        es6: true,
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:prettier/recommended',
-        'plugin:testing-library/react',
-        'plugin:jest-dom/recommended',
-        'plugin:tailwindcss/recommended',
-        'plugin:vitest/legacy-recommended',
-      ],
-      rules: {
-        'import/no-restricted-paths': [
-          'error',
-          {
-            zones: [
-              // disables cross-feature imports:
-              // eg. src/features/discussions should not import from src/features/comments, etc.
-              {
-                target: './src/features/auth',
-                from: './src/features',
-                except: ['./auth'],
-              },
-              {
-                target: './src/features/comments',
-                from: './src/features',
-                except: ['./comments'],
-              },
-              {
-                target: './src/features/discussions',
-                from: './src/features',
-                except: ['./discussions'],
-              },
-              {
-                target: './src/features/teams',
-                from: './src/features',
-                except: ['./teams'],
-              },
-              {
-                target: './src/features/users',
-                from: './src/features',
-                except: ['./users'],
-              },
-              // enforce unidirectional codebase:
-
-              // e.g. src/app can import from src/features but not the other way around
-              {
-                target: './src/features',
-                from: './src/app',
-              },
-
-              // e.g src/features and src/app can import from these shared modules but not the other way around
-              {
-                target: [
-                  './src/components',
-                  './src/hooks',
-                  './src/lib',
-                  './src/types',
-                  './src/utils',
-                ],
-                from: ['./src/features', './src/app'],
-              },
-            ],
-          },
-        ],
-        'import/no-cycle': 'error',
-        'linebreak-style': ['error', 'unix'],
-        'react/prop-types': 'off',
-        'import/order': [
-          'error',
-          {
-            groups: [
-              'builtin',
-              'external',
-              'internal',
-              'parent',
-              'sibling',
-              'index',
-              'object',
-            ],
-            'newlines-between': 'always',
-            alphabetize: { order: 'asc', caseInsensitive: true },
-          },
-        ],
-        'import/default': 'off',
-        'import/no-named-as-default-member': 'off',
-        'import/no-named-as-default': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'jsx-a11y/anchor-is-valid': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/explicit-function-return-type': ['off'],
-        '@typescript-eslint/explicit-module-boundary-types': ['off'],
-        '@typescript-eslint/no-empty-function': ['off'],
-        '@typescript-eslint/no-explicit-any': ['off'],
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-        'check-file/filename-naming-convention': [
-          'error',
-          {
-            '**/*.{ts,tsx}': 'KEBAB_CASE',
-          },
-          {
-            ignoreMiddleExtensions: true,
-          },
-        ],
-      },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Enable JSX syntax
     },
-    {
-      plugins: ['check-file'],
-      files: ['src/**/!(__tests__)/*'],
-      rules: {
-        'check-file/folder-naming-convention': [
-          'error',
-          {
-            '**/*': 'KEBAB_CASE',
-          },
-        ],
-      },
-    },
+    ecmaVersion: 12, // Use ECMAScript 2021 syntax
+    sourceType: 'module', // Allow the use of import/export statements
+  },
+  plugins: [
+    'react', // Enable React plugin
+    'react-hooks', // Enable React Hooks plugin
   ],
+  rules: {
+    'react/prop-types': 'off', // Disable prop-types rule if you're using TypeScript or another method for type-checking
+    'react/react-in-jsx-scope': 'off', // Disable rule requiring React to be in scope when using JSX
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the version of React
+    },
+  },
 };
