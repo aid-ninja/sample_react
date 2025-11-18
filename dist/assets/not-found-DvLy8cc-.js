784666a8e03b93c2b374614e3693bd7103d6d7de
Aid Idrizovic <aid.idrizovic@ninjatrader.com>
2025-11-18 16:33:37 -0600

    ok

---
+++ dist/assets/not-found-DvLy8cc-.js
@@ -0,0 +1,1 @@
+import{j as e,L as o}from"./index-Djl8QRVs.js";const r=()=>e.jsxs("div",{className:"mt-52 flex flex-col items-center font-semibold",children:[e.jsx("h1",{children:"404 - Not Found"}),e.jsx("p",{children:"Sorry, the page you are looking for does not exist."}),e.jsx(o,{to:"/",replace:!0,children:"Go to Home"})]});export{r as NotFoundRoute};
